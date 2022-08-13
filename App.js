832aee087bfca4b84ac3093b02d75ea7b793612a
yusuf963 <mohammad963yusuf@gmail.com>
2022-08-13 20:24:22 +0100

    configure eslint + prettier and add scriopt in package.json file to run fixes

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
-console.log(StatusBar.currentHeight);
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
 
-const isAndroid = Platform.OS === 'android';
-const isIos = Platform.OS === 'ios';
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>
+          Open up App.js to start working on your app!
+        </Text>
         <Text style={styles.text}>Publishing to Expo store</Text>
         <Text style={styles.text}>Publishing to Expo store</Text>
         <ExpoStatusBar style="auto" />
       </View>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
   text: {
-    color: '#000',
-  }
+    color: "#000",
+  },
 });
 export default App;
