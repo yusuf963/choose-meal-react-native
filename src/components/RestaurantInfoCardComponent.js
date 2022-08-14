4ac8b64369bdcdc835a359725b4747a289626efe
yusuf963 <mohammad963yusuf@gmail.com>
2022-08-14 17:58:03 +0100

    add clean code and use spacer component

--- src/components/RestaurantInfoCardComponent.js
+++ src/components/RestaurantInfoCardComponent.js
@@ -15,7 +15,7 @@ import {
 } from "./RestaurantInfoComponent-style";
 import { Text } from "./typography/TextComponent.js";
 
-export const RestaurantsInfo = ({ restaurant = {} }) => {
+export const RestaurantsInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
     icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
--- src/screens/RestaurantsScreen.js
+++ src/screens/RestaurantsScreen.js
@@ -1,18 +1,47 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { FlatList, StatusBar, SafeAreaView } from "react-native";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
+import { RestaurantsInfoCard } from "../components/RestaurantInfoCardComponent";
+import { Spacer } from "../components/spacer/Spacer";
 
-import { RestaurantsInfo } from "../components/RestaurantInfoCardComponent";
-
-const SafeArea = styled.SafeAreaView`
-
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  }
+})``;
+
 export const RestaurantScreen = () => {
   return (
     <SafeArea>
-      <Searchbar placeholder="Search" />
-      <RestaurantsInfo />
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantListContainer>
+        <RestaurantList
+          data={[{ name: 1 }, { name: 2 }, { name: 3 }]}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item }) => (
+            <Spacer position="bottom" size="large">
+              <RestaurantsInfoCard restaurant={item} />
+            </Spacer>
+          )}
+          //eslint-disable-next-line react-native/no-inline-styles 
+          // contentContainerStyle={{ padding: 26 }}
+        />
+      </RestaurantListContainer>
     </SafeArea>
   );
 };
