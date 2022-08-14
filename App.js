211a1f1baf6b3a142be0085bfb5edd4c2f1239b5
yusuf963 <mohammad963yusuf@gmail.com>
2022-08-14 09:15:30 +0100

    convert style to styled component way

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import * as React from "react";
-
+import { StatusBar } from "expo-status-bar";
 import { RestaurantScreen } from "./src/screens/RestaurantsScreen";
 
-export default function App() {
+const App = () => {
   return (
     <>
       <RestaurantScreen />
+      <StatusBar style="auto" />
     </>
   );
-}
+};
+
+export default App;
--- src/components/RestaurantInfoCardComponent.js
+++ src/components/RestaurantInfoCardComponent.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import styled from "styled-components/native";
 import { Card } from "react-native-paper";
+
+const StyledView = styled.View`
+  width: 100%;
+  height: 200px;
+`;
+
+const StyledCardCover = styled(Card.Cover)`
+  padding: 40px;
+  height: 200px;
+`;
 
 export const RestaurantsInfo = () => {
   return (
-    <View>
+    <StyledView>
       <Card elevation={5}>
-        <Card.Cover style={styles.container} source={{
-          uri: "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"
-        }}
+        <StyledCardCover
+          source={{
+            uri: "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+          }}
         />
       </Card>
-    </View>
+    </StyledView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    width: "100%",
-  },
-});
