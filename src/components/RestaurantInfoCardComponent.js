60f153dac66e44812d79712dc29af614391cadd8
yusuf963 <mohammad963yusuf@gmail.com>
2022-08-14 12:43:42 +0100

    add create custom Text typography component andsplit style function to style file of resturant component

--- src/components/RestaurantInfoCardComponent.js
+++ src/components/RestaurantInfoCardComponent.js
@@ -1,49 +1,21 @@
 import React from "react";
-import { Text, Image } from "react-native";
-import styled from "styled-components/native";
-import { Card } from "react-native-paper";
+// import { Text, Image } from "react-native";
 import { SvgXml } from "react-native-svg";
+import { Image } from "react-native";
 import star from "../../assets/star.js";
 import open from "../../assets/open.js";
 import { Spacer } from "./spacer/Spacer";
-
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Title,
+  Rating,
+  Section,
+  SectionEnd,
+  Address,
+} from "./RestaurantInfoComponent-style";
+import { Text } from "./typography/TextComponent.js";
 
 export const RestaurantsInfo = ({ restaurant = {} }) => {
   const {
@@ -64,18 +36,16 @@ export const RestaurantsInfo = ({ restaurant = {} }) => {
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
-        <Title>{name}</Title>
+        <Text variant="label">{name}</Text>
         <Section>
           <Rating>
-            {rateStars.map(() => (
-              <SvgXml xml={star} width={20} height={20} />
+            {rateStars.map((_, index) => (
+              <SvgXml key={index} xml={star} width={20} height={20} />
             ))}
           </Rating>
           <SectionEnd>
             {isClosedTemporarily && (
-              <Text variant="label" style={{ color: "red" }}>
-                CLOSED TEMPORARILY
-              </Text>
+              <Text variant="error">CLOSED TEMPORARILY</Text>
             )}
             <Spacer position="left" size="large">
               {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
---
+++ src/components/RestaurantInfoComponent-style.js
@@ -0,0 +1,40 @@
+import styled from "styled-components/native";
+import { Card, Text } from "react-native-paper";
+
+export const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+export const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+export const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+export const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+export const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
